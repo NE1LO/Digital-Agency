9207550c120ab99a476004ab806a3d1d59a0c6aa
Anton <131876090+NE1LO@users.noreply.github.com>
2024-01-07 01:14:43 +0100

    update

--- js/main.js
+++ js/main.js
@@ -2,5 +2,5 @@ const FormEl = document.querySelector(".feedback-form");
 
 FormEl.addEventListener("submit", (e) => {
   e.preventDefault();
-  alert("sorry we can't submit this form X (");
+  alert("sorry we can't submit this form X(");
 });
